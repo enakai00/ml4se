8a0ef7be913beaf0f3481a0ff790763b2e7f4766
Etsuji Nakai <enakai@redhat.com>
2015-05-21 09:14:50 +0900

    Add: change backend to TkAgg

--- config.sh
+++ config.sh
@@ -5,5 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pandas import Series, DataFrame
 EOF
+sed -i 's/^backend\s*:.*$/backend      : TkAgg/' \
+~/Enthought/Canopy_*bit/User/lib/python*/site-packages/matplotlib/mpl-data/matplotlibrc
 . ~/.bashrc
 echo Done.
